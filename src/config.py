ba6bb169bfaf526e18729c7d24246ddc8ebe31e5
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-02-09 17:13:16 +1100

    fix: fixed the rest of the type errors in the project

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ _ = dotenv.load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_DIR = os.getenv("DB_DIR")
-MY_USER_ID = int(os.getenv("MY_USER_ID"))
-MY_GUILD_ID = int(os.getenv("MY_GUILD_ID"))
+MY_USER_ID = int(os.getenv("MY_USER_ID", "0"))
+MY_GUILD_ID = int(os.getenv("MY_GUILD_ID", "0"))
